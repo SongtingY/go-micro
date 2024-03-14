79294948e55a3651c87c2f5b72dfd0be6d05e993
SongtingY <yang.song@northeastern.edu>
2024-03-14 16:22:26 -0700

    part of the logger

---
+++ logger-service/data/model.go
@@ -0,0 +1,32 @@
+package data
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func (l *LogEntry) Insert(entry LogEntry) error {
+
+}
